5547365e82bf496008d9cc03947e482cbefd72b8
Andrius Urbelis <andrius.urbelis@coxautoinc.com>
2024-01-08 21:25:21 +0200

    client comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import styles from './page.module.css'
+import Counter from './components/counter'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <span>Hi Next.js v2</span>
+      <Counter />
     </main>
   )
 }
