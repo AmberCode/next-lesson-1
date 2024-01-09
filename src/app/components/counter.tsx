b8f9a8aa4ddfb227d34c188f951e52470bebc7c2
Andrius Urbelis <andrius.urbelis@coxautoinc.com>
2024-01-09 11:22:09 +0200

    unit tests

--- src/app/components/counter.tsx
+++ src/app/components/counter.tsx
@@ -7,7 +7,7 @@ export default function Counter() {
   return (
     <div>
       <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click Me</button>
+      <button data-testid='buttonCount' onClick={() => setCount(count + 1)}>Click Me</button>
     </div>
   )
 }
